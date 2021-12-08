1c0153b113e41df875061f79a26a0bb0fa8f4ee4
jonasdiego26 <jonasvandoorn26@gmail.com>
2021-12-08 15:40:43 +0100

    UI of Create article page

---
+++ app/src/main/java/com/example/newsapplication/CreateActivity.java
@@ -0,0 +1,21 @@
+package com.example.newsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_article);
+
+    }
+
+}
